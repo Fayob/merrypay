6b917132e0fdd31276d64598c9752bec9f013cbc
fayob <adedejifavour6@gmail.com>
2023-11-01 20:00:38 +0100

    Add model to db

---
+++ db/model.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+type SQLQueries interface {
+	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+	PrepareContext(context.Context, string) (*sql.Stmt, error)
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+}
