b67ca4882ab20c7f2e69fd7f93260766aca752e4
fayob <adedejifavour6@gmail.com>
2023-11-01 20:02:34 +0100

    Add find user functionality

--- service/user.go
+++ service/user.go
@@ -2,8 +2,19 @@ package service
 
 import (
 	"context"
+	"time"
 )
 
+type User struct {
+	Username          string    `json:"username"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	Email             string    `json:"email"`
+	Membership        string    `json:"membership"`
+	Password          string    `json:"password"`
+	UpdatedPasswordAt time.Time `json:"updated_password_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
 type CreateUserParams struct {
 	Username  string `json:"username"`
 	FirstName string `json:"first_name"`
@@ -24,3 +35,20 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (string,
 	return "User Created Successfully", err
 }
 
+func (q *Queries) FindUser(ctx context.Context, arg string) (User, error) {
+	var user User
+	query := `SELECT username, first_name, last_name, email, membership, password, updated_password_at, created_at
+						FROM users where username = $1 or email = $1`
+	res := q.db.QueryRowContext(ctx, query, arg)
+	err := res.Scan(
+		&user.Username,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Membership,
+		&user.Password,
+		&user.UpdatedPasswordAt,
+		&user.CreatedAt,
+	)
+	return user, err
+}
