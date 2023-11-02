549f3a9642c160a4a7412928039d8e58693f6e30
fayob <adedejifavour6@gmail.com>
2023-11-02 12:15:07 +0100

    Add find all user functionality

--- service/user.go
+++ service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -27,11 +28,17 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (string,
 	_, err := q.db.ExecContext(ctx, `INSERT INTO users(username, first_name, last_name, email, password)
 	VALUES($1, $2, $3, $4, $5)`, arg.Username, arg.FirstName, arg.LastName, arg.Email, arg.Password)
 
-
 	if err != nil {
+		// 	if pqErr, ok := err.(*pq.Error); ok {
+		// 		if pqErr.Constraint == "users_email_key" {
+		// 			fmt.Println(pqErr.Code)
+		// 			return "", fmt.Errorf("email already in use")
+		// 		}
+		// 	}
 		return "", err
 	}
 
+	fmt.Println("User Created Successfully")
 	return "User Created Successfully", err
 }
 
@@ -51,4 +58,31 @@ func (q *Queries) FindUser(ctx context.Context, arg string) (User, error) {
 		&user.CreatedAt,
 	)
 	return user, err
+}
+
+func (q *Queries) FindAllUsers(ctx context.Context) ([]User, error) {
+	query := `SELECT username, first_name, last_name, email, membership, 
+							password, updated_password_at, created_at FROM users`
+
+	rows, err := q.db.QueryContext(ctx, query)
+	var users []User
+
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(
+			&user.Username,
+			&user.FirstName,
+			&user.LastName,
+			&user.Email,
+			&user.Membership,
+			&user.Password,
+			&user.UpdatedPasswordAt,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, err
 }
