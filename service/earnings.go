092aa9a1215b81524721057dcd3c2ad087481125
fayob <adedejifavour6@gmail.com>
2023-11-07 09:02:43 +0100

    Add update to earning service

--- service/earnings.go
+++ service/earnings.go
@@ -32,3 +32,32 @@ func (q *Queries) GetEarning(ctx context.Context, owner string) (types.Earning, 
 
 	return earning, err
 }
+
+type UpdateEarningParams struct {
+	Referrals            int    `json:"referrals"`
+	ReferralBalance      int    `json:"referral_balance"`
+	ReferralTotalEarning int    `json:"referral_total_earning"`
+	TotalWithdrawal      int    `json:"total_withdrawal"`
+	MediaEarning         int    `json:"media_earning"`
+	Owner                string `json:"owner"`
+}
+
+func (q *Queries) UpdateEarning(ctx context.Context, arg UpdateEarningParams) (types.Earning, error) {
+	query := `UPDATE earnings SET referrals = $2, referral_balance = $3, referral_total_earning = $4,
+						total_withdrawal = $5, media_earning = $6 where owner = $1 RETURNING id, referrals, 
+						referral_balance, referral_total_earning, total_withdrawal, media_earning, owner`
+
+	row := q.db.QueryRowContext(ctx, query, arg.Owner, arg.Referrals, arg.ReferralBalance, arg.ReferralTotalEarning, arg.TotalWithdrawal, arg.MediaEarning)
+	var earning types.Earning
+	err := row.Scan(
+		&earning.ID,
+		&earning.Referrals,
+		&earning.ReferralBalance,
+		&earning.ReferralTotalEarning,
+		&earning.TotalWithdrawal,
+		&earning.MediaEarning,
+		&earning.Owner,
+	)
+
+	return earning, err
+}
