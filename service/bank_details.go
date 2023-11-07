2a6bcf83e8d09033dcc1fa2e1f0439c493125446
fayob <adedejifavour6@gmail.com>
2023-11-07 09:01:03 +0100

    Add update to bank detail

--- service/bank_details.go
+++ service/bank_details.go
@@ -38,3 +38,28 @@ func (q *Queries) FetchBankDetail(ctx context.Context, owner string) (types.Bank
 
 	return bankDetail, err
 }
+
+func (q *Queries) UpdateBankDetail(ctx context.Context, arg BankDetailParams) (types.BankDetail, error) {
+	query := `UPDATE bank_details SET bank_name = $1, account_name = $2, account_number = $3 where owner = $4
+						RETURNING id, bank_name, account_number, account_name, owner, created_at`
+	row := q.db.QueryRowContext(ctx, query, arg.BankName, arg.AccountName, arg.AccountNumber, arg.Owner)
+	var bankDetail types.BankDetail
+	err := row.Scan(
+		&bankDetail.ID,
+		&bankDetail.BankName,
+		&bankDetail.AccountNumber,
+		&bankDetail.AccountName,
+		&bankDetail.Owner,
+		&bankDetail.CreatedAt,
+	)
+	return bankDetail, err
+}
+
+func (q *Queries) UpdateBankDetailNoReturn(ctx context.Context, arg BankDetailParams) error {
+	query := `UPDATE bank_details SET bank_name = $1, account_name = $2, account_number = $3 where owner = $4`
+	_, err := q.db.ExecContext(ctx, query, arg.BankName, arg.AccountName, arg.AccountNumber, arg.Owner)
+	if err != nil {
+		return err
+	}
+	return nil
+}
