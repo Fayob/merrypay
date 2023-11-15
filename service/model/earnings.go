6a43b1396f3fb3fd01440ed576086e1ba1526a0e
fayob <adedejifavour6@gmail.com>
2023-11-15 16:38:32 +0100

    Update earning model

--- service/model/earnings.go
+++ service/model/earnings.go
@@ -15,8 +15,8 @@ func (q *Queries) CreateEarning(ctx context.Context, owner string) error {
 }
 
 func (q *Queries) GetEarning(ctx context.Context, owner string) (types.Earning, error) {
-	query := `SELECT id, referrals, referral_balance, referral_total_earning, total_withdrawal, media_earning,
-						owner FROM earnings WHERE owner = $1`
+	query := `SELECT id, referrals, referral_balance, referral_total_earning, referral_total_withdrawal, 
+				media_balance, media_total_earning, media_total_withdrawal, owner FROM earnings WHERE owner = $1`
 
 	row := q.db.QueryRowContext(ctx, query, owner)
 	var earning types.Earning
@@ -25,8 +25,10 @@ func (q *Queries) GetEarning(ctx context.Context, owner string) (types.Earning, 
 		&earning.Referrals,
 		&earning.ReferralBalance,
 		&earning.ReferralTotalEarning,
-		&earning.TotalWithdrawal,
-		&earning.MediaEarning,
+		&earning.ReferralTotalWithdrawal,
+		&earning.MediaBalance,
+		&earning.MediaTotalEarning,
+		&earning.MediaTotalWithdrawal,
 		&earning.Owner,
 	)
 
@@ -35,18 +37,26 @@ func (q *Queries) GetEarning(ctx context.Context, owner string) (types.Earning, 
 
 func (q *Queries) UpdateEarning(ctx context.Context, arg types.UpdateEarningParams) (types.Earning, error) {
 	query := `UPDATE earnings SET referrals = $2, referral_balance = $3, referral_total_earning = $4,
-						total_withdrawal = $5, media_earning = $6 where owner = $1 RETURNING id, referrals, 
-						referral_balance, referral_total_earning, total_withdrawal, media_earning, owner`
+						referral_total_withdrawal = $5, media_balance = $6, media_total_earning = $7,
+						media_total_withdrawal = $8 where owner = $1 RETURNING id, referrals, referral_balance, 
+						referral_total_earning, referral_total_withdrawal, media_balance, media_total_earning, 
+						media_total_withdrawal, owner`
 
-	row := q.db.QueryRowContext(ctx, query, arg.Owner, arg.Referrals, arg.ReferralBalance, arg.ReferralTotalEarning, arg.TotalWithdrawal, arg.MediaEarning)
+	row := q.db.QueryRowContext(
+		ctx, query, arg.Owner, arg.Referrals, arg.ReferralBalance, 
+		arg.ReferralTotalEarning, arg.ReferralTotalWithdrawal, arg.MediaBalance,
+		arg.MediaTotalEarning, arg.MediaTotalWithdrawal,
+	)
 	var earning types.Earning
 	err := row.Scan(
 		&earning.ID,
 		&earning.Referrals,
 		&earning.ReferralBalance,
 		&earning.ReferralTotalEarning,
-		&earning.TotalWithdrawal,
-		&earning.MediaEarning,
+		&earning.ReferralTotalWithdrawal,
+		&earning.MediaBalance,
+		&earning.MediaTotalEarning,
+		&earning.MediaTotalWithdrawal,
 		&earning.Owner,
 	)
 
