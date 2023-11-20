128c35221d62ffaa4473058318ba9879c94472d2
fayob <adedejifavour6@gmail.com>
2023-11-20 11:23:53 +0100

    Get Transaction by id

--- service/model/transactions.go
+++ service/model/transactions.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"merrypay/types"
 )
 
 type CreateTransaction struct {
@@ -24,3 +25,17 @@ func (q *Queries) CreateTransaction(ctx context.Context, arg CreateTransaction) 
 // 	return err
 // }
 
+func (q *Queries) GetTransactionByID(ctx context.Context, id int) (types.Transaction, error) {
+	query := `SELECT id, amount, kind, transact_by, created_at FROM transactions WHERE id=$1`
+	row := q.db.QueryRowContext(ctx, query, id)
+	var transaction types.Transaction
+	err := row.Scan(
+		&transaction.ID,
+		&transaction.Amount,
+		&transaction.Kind,
+		&transaction.TransactBy,
+		&transaction.CreatedAt,
+	)
+
+	return transaction, err
+}
