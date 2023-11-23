0f6e9610f2e4c1a8bb4f452d883d5b1a66f2a279
fayob <adedejifavour6@gmail.com>
2023-11-23 14:47:39 +0100

    Add update user functionality

--- service/repository/user.go
+++ service/repository/user.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"merrypay/types"
 )
@@ -17,4 +18,30 @@ func (m *Model) GetUser(ctx context.Context, username string) (types.User, error
 	}
 
 	return user, nil
+}
+
+func (m *Model) UpdateUser(ctx context.Context, arg types.UpdateUserParams) error {
+	if arg.Username == "" || arg.Email == "" || arg.FirstName == "" || arg.LastName == "" {
+		return fmt.Errorf("all field must be filled")
+	}
+
+	user, err := m.Model.FindUser(ctx, arg.Username)
+	if err != nil {
+		return err
+	}
+
+	if user.Email != arg.Email {
+		_, err := m.Model.FindUser(ctx, arg.Email)
+		if err != sql.ErrNoRows {
+			fmt.Println(err)
+			return fmt.Errorf("email already in use please choose another email address")
+		}
+	}
+
+	_, err = m.Model.UpdateUser(ctx, arg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
