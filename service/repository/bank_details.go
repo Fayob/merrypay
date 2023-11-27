d289be1514c684569c4817c8c1672d3d9c89e6c5
fayob <adedejifavour6@gmail.com>
2023-11-27 11:02:50 +0100

    Fetch bank details from server

--- service/repository/bank_details.go
+++ service/repository/bank_details.go
@@ -15,3 +15,16 @@ func (m *Model) AddBankDetails(ctx context.Context, arg types.BankDetailParams) 
 
 	// return err
 }
+
+func (m *Model) GetBankDetails(ctx context.Context, username string) (types.BankDetail, error) {
+	if username == "" {
+		return types.BankDetail{}, fmt.Errorf("please supply username")
+	}
+
+	bankDetail, err := m.Model.FetchBankDetail(ctx, username)
+	if err != nil {
+		return types.BankDetail{}, err
+	}
+
+	return bankDetail, nil
+}
