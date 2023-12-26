0f8693ee478a12df58e94a371507122fff4b648d
fayob <adedejifavour6@gmail.com>
2023-12-26 17:42:48 +0100

    Update store

--- port/store.go
+++ port/store.go
@@ -10,28 +10,26 @@ type Store interface {
 	CreateUser(context.Context, types.CreateUserParams) (types.User, error)
 	FindUser(context.Context, string) (types.User, error)
 	FindAllUsers(context.Context) ([]types.User, error)
-	UpdateUser(context.Context, service.UpdateUserParams) (types.User, error)
-	UpdatePassword(context.Context, string, string) (string, error)
+	UpdateUser(context.Context, types.UpdateUserParams) (types.User, error)
+	UpdatePassword(ctx context.Context, password, identifier string) (string, error)
 	DeleteUser(context.Context, string) (string, error)
+	ReferralHistory(ctx context.Context, username string) ([]types.RefHisResponse, error)
 	SaveCoupon(context.Context, string, string) (string, error)
 	RegisterWithCoupon(context.Context, string, string) error
 	GetCoupon(context.Context, string) (types.Coupon, error)
-	CreateEarning(ctx context.Context, owner string) error
+	CreateEarning(ctx context.Context, owner string) (types.Earning, error)
 	GetEarning(ctx context.Context, owner string) (types.Earning, error)
 	UpdateEarning(ctx context.Context, arg types.UpdateEarningParams) (types.Earning, error)
-	SaveBankDetails(ctx context.Context, arg service.BankDetailParams) error
+	SaveBankDetails(ctx context.Context, arg types.BankDetailParams) error
 	FetchBankDetail(ctx context.Context, owner string) (types.BankDetail, error)
-	UpdateBankDetail(ctx context.Context, arg service.BankDetailParams) (types.BankDetail, error)
+	UpdateBankDetail(ctx context.Context, arg types.BankDetailParams) (types.BankDetail, error)
 	InitiateWithdrawal(ctx context.Context, arg service.InitiateWithdrawalParams) (types.Withdrawal, error)
 	UpdateWithdrawal(ctx context.Context, id int, status string) (types.Withdrawal, error)
 	GetWithdrawalByID(ctx context.Context, id int) (types.Withdrawal, error)
 	GetUserWithdrawals(ctx context.Context, username string) ([]types.Withdrawal, error)
+	GetWithdrawalsByStatus(ctx context.Context, status string) ([]types.Withdrawal, error)
 	CreateTransaction(ctx context.Context, arg service.CreateTransaction) error
 	GetBalanceFromTransaction(ctx context.Context, username, kind string) (service.Balance, error)
-}
-
-type Handler interface {
-
 }
 
 // type UserService interface {
