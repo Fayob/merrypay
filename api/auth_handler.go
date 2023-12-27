e24f42234ed5459dedc956d900ef58f86992b80d
fayob <adedejifavour6@gmail.com>
2023-12-27 11:28:14 +0100

    Add login handler

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -67,3 +67,35 @@ func (s *Server) SignUp(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, response)
 }
+
+type loginParams struct {
+	Identifier string `json:"username" binding:"required"`
+	Password   string `json:"password" binding:"required"`
+}
+
+func (s *Server) Login(ctx *gin.Context) {
+	var req loginParams
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorFormat(err.Error()))
+		return
+	}
+
+	user, err := s.Server.LogInUser(ctx, req.Identifier, req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorFormat(err.Error()))
+		return
+	}
+
+	token, err := s.tokenMaker.CreateToken(user.Username, user.Membership, time.Hour*4)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorFormat(err.Error()))
+		return
+	}
+
+	response := gin.H{
+		"token": token,
+		"user":  userResponseFunc(user),
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
