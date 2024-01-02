8723d89cfb7676c4eafcceaf66760d858eeb0d7a
fayob <adedejifavour6@gmail.com>
2024-01-02 12:38:52 +0100

    Add handler to update user profile

--- api/user_handler.go
+++ api/user_handler.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"merrypay/token"
+	"merrypay/types"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -27,4 +28,26 @@ func (s *Server) GetUserProfile(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, user)
+}
+
+func (s *Server) UpdateUserProfile(ctx *gin.Context) {
+	var req types.UpdateUserParams
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorFormat(err.Error()))
+		return
+	}
+
+	payload := ctx.MustGet(AuthorizationPayloadKey).(*token.Payload)
+	if payload.Username != req.Username && payload.Membership != "admin" {
+		ctx.JSON(http.StatusUnauthorized, errorFormat("unauthorized route"))
+		return
+	}
+
+	err := s.Server.UpdateUser(ctx, req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorFormat(err.Error()))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "User Updated Successfully")
 }
