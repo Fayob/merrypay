499c860374ea1c994f75734aff63c3f3c1e5977c
fayob <adedejifavour6@gmail.com>
2024-02-08 10:08:05 +0100

    Add withdrawal completion functionality

--- api/funds.go
+++ api/funds.go
@@ -4,6 +4,7 @@ import (
 	"merrypay/token"
 	"merrypay/types"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,3 +28,29 @@ func (s *Server) InitiateWithdrawal(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, withdrawal)
 }
 
+func (s *Server) CompleteWithdrawal(ctx *gin.Context) {
+	withdrawalID, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorFormat(err.Error()))
+		return
+	}
+	// var req types.CompleteWithdrawalParams
+	// if err := ctx.ShouldBindJSON(&req); err != nil {
+	// 	ctx.JSON(http.StatusBadRequest, errorFormat(err.Error()))
+	// 	return
+	// }
+
+	payload := ctx.MustGet(AuthorizationPayloadKey).(*token.Payload)
+	if payload.Membership != "admin" {
+		ctx.JSON(http.StatusUnauthorized, errorFormat("unauthorized route"))
+		return
+	}
+
+	err = s.Server.CompleteWithdrawal(ctx, withdrawalID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorFormat(err.Error()))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "Withdrawal Completed")
+}
