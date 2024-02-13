61271ac043c1e13c5e3ae2be6293defa58dc04ce
fayob <adedejifavour6@gmail.com>
2024-02-13 11:06:38 +0100

    Add get withdrawal functionalities

--- api/funds.go
+++ api/funds.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"merrypay/token"
 	"merrypay/types"
 	"net/http"
@@ -72,4 +73,38 @@ func (s *Server) Jackpot(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, result)
+}
+
+func (s *Server) GetWithdrawalByID(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		fmt.Println("unable to convert params id to int")
+		return
+	}
+
+	withdrawal, err := s.Server.GetWithdrawReceiptByID(ctx, id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorFormat(err.Error()))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, withdrawal)
+}
+
+func (s *Server) GetWithdrawalsByStatus(ctx *gin.Context) {
+	payload := ctx.MustGet(AuthorizationPayloadKey).(*token.Payload)
+	if payload.Membership != "admin" {
+		ctx.JSON(http.StatusUnauthorized, "unauthorized route")
+		return
+	}
+	status := ctx.Param("status")
+
+	withdrawal, err := s.Server.GetWithdrawalByStatus(ctx, status)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorFormat(err.Error()))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, withdrawal)
 }
